a10fb0a23e65c5c5943626a5d88479dc35c029db
Arash Mehdipour <arash201221@gmail.com>
2021-10-08 10:12:39 +0330

    Add project files.

---
+++ IdentitySamples/Models/AAA/Data/AAADbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentitySamples.Models.AAA.Data
+{
+    public class AAADbContext : IdentityDbContext<IdentityUser>
+    {
+        public AAADbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
