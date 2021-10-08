70aacedbfc63e2725e489704b02012311474c461
Arash Mehdipour <arash201221@gmail.com>
2021-10-08 11:41:53 +0330

    Add-User

---
+++ IdentitySamples/Controllers/UsersController.cs
@@ -0,0 +1,58 @@
+﻿using IdentitySamples.Models.AAA.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentitySamples.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UsersController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            var users = _userManager.Users.ToList();
+            return View(users);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateUserModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var identityUser = new IdentityUser()
+                {
+                    UserName = model.UserName,
+                    Email = model.Email
+                };
+                var result = await _userManager.CreateAsync(identityUser, model.Password);
+                if (result.Succeeded)
+                {
+                    TempData["Message"] = "User Created";
+                    return RedirectToAction("Index","Users");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ IdentitySamples/Models/AAA/Data/CreateUserModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentitySamples.Models.AAA.Data
+{
+    public class CreateUserModel
+    {
+        [Required]
+        public string UserName { get; set; }
+        
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
